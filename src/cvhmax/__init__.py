5da45eca11dead7e163c9e302f02e5b4426d94ae
yuanz <yuanz271@gmail.com>
2026-01-19 14:43:24 -0500

    export public API from package root

--- src/cvhmax/__init__.py
+++ src/cvhmax/__init__.py
@@ -1,3 +1,10 @@
 # SPDX-FileCopyrightText: 2023-present yuanz <yuanz271@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+
+from .cvhm import CVHM
+from .cvi import CVI, Gaussian, Poisson, Params
+from .hm import HidaMatern
+from .hp import whittle
+
+__all__ = ["CVHM", "CVI", "Gaussian", "Poisson", "Params", "HidaMatern", "whittle"]
