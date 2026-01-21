a585a991af73c6ac465decc07f89d5b94003c4aa
yuanz <yuanz271@gmail.com>
2026-01-21 10:06:53 -0500

    remove SPDX headers from __init__.py files

--- src/cvhmax/__init__.py
+++ src/cvhmax/__init__.py
@@ -1,7 +1,3 @@
-# SPDX-FileCopyrightText: 2023-present yuanz <yuanz271@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-
 from .cvhm import CVHM
 from .cvi import CVI, Gaussian, Poisson, Params
 from .hm import HidaMatern
